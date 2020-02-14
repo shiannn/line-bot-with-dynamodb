ea7a4c2682f4500245062535ae91f5fd09c95efd
shiannn <b05502087@ntu.edu.tw>
2020-02-14 17:34:22 +0800

    Finish 1. store only the root page into DynamoDB
    Todo
    1. match the keyword with the html
    2. DFS

--- DynamoDBcode/listTable.py
+++ DynamoDBcode/listTable.py
@@ -15,6 +15,8 @@ def scanTable(ddb, TableNames):
 if __name__ == '__main__':
     ddb_client = boto3.client('dynamodb', endpoint_url='http://localhost:8000')
     ddb_resource = boto3.resource('dynamodb', region_name='us-west-2', endpoint_url="http://localhost:8000")
-    TableNames = 'MessageTable'
+    #TableNames = 'MessageTable'
+    TableNames = 'ParseInfoTable'
+    
     scanTable(ddb_resource, TableNames)
     #listTables(ddb_client)
